a8fe74cbd6a97d4492f91ec60b8f65fc38c4f221
Richard Lindner <richard.lnnr@gmail.com>
2018-01-30 17:53:48 -0200

    Teste para rotas do Heroku

--- Server.js
+++ Server.js
@@ -25,6 +25,10 @@ app.use(forceSSL());
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+
+app.route('/*').get(function(req, res) { 
+  return res.sendFile(path.join(config.root, 'index.html')); 
+});
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
